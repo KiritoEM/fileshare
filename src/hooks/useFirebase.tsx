755a16fa4ae54f40afe59fe2418a84321b12a898
kiritoEM <johankirito64@gmail.com>
2024-01-04 08:14:43 +0100

    add firebase hooks

---
+++ src/hooks/useFirebase.tsx
@@ -0,0 +1,66 @@
+import React, { ReactNode, createContext, useContext, useState } from "react";
+import { auth, provider } from "../config/firebase-config";
+import { signInWithPopup, signOut } from "firebase/auth";
+
+export interface IFirebaseContext {
+  setGooglePopup: () => void;
+  setLogout: () => void;
+  emailSent: string | null;
+  setEmailValue: (email: string | null) => void;
+}
+
+interface IAuthProvider {
+  children?: ReactNode;
+}
+
+const FirebaseContext = createContext<IFirebaseContext | null>(null);
+
+export const FirebaseProvider: React.FC<IAuthProvider> = ({ children }) => {
+  const [emailSent, sentEmail] = useState<string | null>("");
+
+  //show Google popup
+  const setGooglePopup = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      if (data.user.email) {
+        localStorage.setItem("userEmail", data.user.email);
+        sentEmail(data.user.email);
+      }
+    });
+  };
+
+  const setEmailValue = (email: string | null) => {
+    if (typeof window !== "undefined") {
+      sentEmail(email);
+    }
+    return email;
+  };
+
+  const setLogout = () => {
+    signOut(auth).then(() => {
+      localStorage.removeItem("userEmail");
+    });
+  };
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        emailSent,
+        setGooglePopup,
+        setLogout,
+        setEmailValue,
+      }}
+    >
+      {children}
+    </FirebaseContext.Provider>
+  );
+};
+
+//hooks exportation
+export const useFirebase = () => {
+  const context = useContext(FirebaseContext);
+
+  if (!context) {
+    throw new Error("useFirebase must be used within an AppProvider");
+  }
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { FirebaseProvider } from "./hooks/useFirebase.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
+  </React.StrictMode>
+);
