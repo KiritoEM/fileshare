f5e01f42e1d38c6c6072f2048396b216e61c78b9
kiritoEM <johankirito64@gmail.com>
2024-01-04 07:31:43 +0100

    add button to login-connect

--- src/components/Login/LoginConnect.tsx
+++ src/components/Login/LoginConnect.tsx
@@ -1,11 +1,24 @@
 const LoginConnect = () => {
-    return (
-        <section id="login-connect">
-            <button className="btn btn-secondary">
-                  Se connecter avec Google
-            </button>
-        </section>
-    );
+  return (
+    <section id="login-connect">
+      <div className="btn-google">
+        <button className="btn btn-1">
+          <div className="img">
+            <img src="/google.png" alt="" />
+          </div>
+          Se connecter avec Google
+        </button>
+      </div>
+      <div className="btn-google">
+        <button className="btn btn-2">
+          <div className="img">
+            <img src="/facebook.png" alt="" />
+          </div>
+          Se connecter avec Facebook
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default LoginConnect;
